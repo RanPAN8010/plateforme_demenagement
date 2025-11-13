9627a00709498630d213f3bedcc0822b8ad5436a
ZHOU YIHUA <zhouyihua258@gmail.com>
2025-11-13 10:44:33 +0100

    导航

---
+++ head.php
@@ -0,0 +1,16 @@
+<header class="navbar">
+    <div class="logo">
+        <a href="index.php">
+        <img src="img/logo.png" alt="Logo" class="logo-img">
+    </a></div>
+    
+    <nav>
+        <ul class="nav-links">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="ads-list.php">Liste des annonces</a></li>
+            <li><a href="#">Inscription déménageur</a></li>
+            <li><a href="publish-ad.php">Publier une annonce</a></li>
+            <li><a href="#">Se connecter</a></li>
+        </ul>
+    </nav>
+</header>
